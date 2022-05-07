8a232a4c25a792226bf7e3bb97233e4124d5ed87
Yogeshshrestha <yogeshshrestha845@gmail.com>
2022-05-07 19:08:25 +0545

    Collabration works

---
+++ collab/final/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.home, name='index.html')
+    # path('post',views.post , name="tax.html")  
+]
---
+++ collab/final/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse    
+
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        sal = request.POST['salary']
+        bon = request.POST['Bonus']
+        allo = request.POST['allowance']
+        oth = request.POST['others']
+        salary = int(sal)
+        bonus = int(bon)
+        allow = int(allo)
+        other = int(oth)
+        total_salary = salary + bonus + allow + other
+        
+        print("salary",salary)
+        print("allowance",allow)
+        print("bonus",bonus)
+        print("other",other)
+        print("total_salary",total_salary)
+
+        if total_salary <=400000:
+            total_tax= total_salary*(0.01)
+            print("total_tax",total_tax)
+        elif total_salary > 400000 and total_salary <=500000:
+            total_tax = total_salary*(0.1)
+            print("total_tax",total_tax)
+        elif total_salary > 500000 and total_salary <=700000:
+            total_tax = total_salary*(0.2)
+            print("total_tax",total_tax)
+        elif total_salary > 700000 and total_salary <=2000000:
+            total_tax = total_salary*(0.3)
+            print("total_tax",total_tax)
+        else:
+            total_tax= total_salary*(0.36)
+            print("total_tax",total_tax)
+    return render(request, 'index.html',{'total_tax':total_tax})
+       
+
+   
+
